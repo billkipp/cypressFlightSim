7d324be82b73cfe0648bf6a5aa6a010f76bbc7ad
billkipp <bill.g.kipp@gmail.com>
2026-02-17 18:16:24 -0600

    B_Refactor_Flight_Sim
    Cleaning up the Flight Sime Repository.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    baseUrl: "https://flightsim.to",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
