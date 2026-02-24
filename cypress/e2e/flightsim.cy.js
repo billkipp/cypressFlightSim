0ba199b88bf1687a8a24bd789a7a651767db1a64
billkipp <bill.g.kipp@gmail.com>
2026-02-24 14:21:37 -0600

    Br adding more cities
    added more cities

--- cypress/e2e/flightsim.cy.js
+++ cypress/e2e/flightsim.cy.js
@@ -15,8 +15,8 @@ describe("FlightSim.to homepage", () => {
 
   it("verifies Sceneries navigation exists for the united states", () => {
 
-    //cy.visit("https://flightsim.to/by-country/US/search/mckinney");
     cy.visit("/");
+
 
     cy.contains("Sceneries", { matchCase: false })
       .should("be.visible")
@@ -24,12 +24,12 @@ describe("FlightSim.to homepage", () => {
   
    
 
-    cy.contains("a", "Sceneries")
+    /*cy.contains("a", "Sceneries")
       .trigger("mouseover", { force: true })
       .trigger("mouseenter", { force: true });
 
 
-    cy.contains("a", "Sceneries")   // or the submenu item text
+      cy.contains("a", "Sceneries")   // or the submenu item text
       .should("be.visible")
       .dblclick()
       .realHover()
@@ -42,14 +42,55 @@ describe("FlightSim.to homepage", () => {
     cy.get('#search').type('McKinney');
   
     
-   cy.get('#search').closest('form').submit();
+    cy.get('#search').closest('form').submit();*/
+
+    
+    cy.visit("https://flightsim.to/by-country/US/search/mckinney");
 
     cy.get('div.tile-grid.results')
-      .children()
-      .should('have.length', 0);
+     .children()
+     .should('have.length', 0);
+
+    cy.wait(2000);
+
+    cy.visit("https://flightsim.to/by-country/US/search/Plano");
+
+    cy.get('div.tile-grid.results')
+     .children()
+     .should('have.length', 0);
+
+    cy.visit("https://flightsim.to/by-country/US/search/Denton");
+    
+    cy.get('div.tile-grid.results')
+     .children()
+     .should('have.length', 1);
+
+    
+    cy.visit("https://flightsim.to/by-country/US/search/Frisco");
+    
+    cy.get('div.tile-grid.results')
+     .children()
+     .should('have.length', 0);
+
+    cy.visit("https://flightsim.to/by-country/US/search/Richardson");
+    
+    cy.get('div.tile-grid.results')
+     .children()
+     .should('have.length', 1);
+
+    cy.visit("https://flightsim.to/by-country/US/search/Allen");
+    
+    cy.get('div.tile-grid.results')
+     .children()
+     .should('have.length', 10);
+
+    cy.visit("https://flightsim.to/by-country/US/search/Little+Elm");
+    
+    cy.get('div.tile-grid.results')
+     .children()
+     .should('have.length', 10);
 
 
   });
-
 
 });
